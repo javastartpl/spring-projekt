716f630d3261e38fb1170f82ed2729ef6f53ba42
Slawek Ludwiczak <slawekwch@gmail.com>
2023-02-07 13:16:14 +0100

    Spring Boot 3 - Lekcja implementacja rejestracji

--- 20-movieclub-rejestracja/src/main/java/pl/javastart/movieclub/config/security/CustomSecurityConfig.java
+++ 20-movieclub-rejestracja/src/main/java/pl/javastart/movieclub/config/security/CustomSecurityConfig.java
@@ -11,14 +11,14 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-class CustomSecurityConfig {
+public class CustomSecurityConfig {
     private static final String USER_ROLE = "USER";
     private static final String EDITOR_ROLE = "EDITOR";
     private static final String ADMIN_ROLE = "ADMIN";
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authz) -> authz
-                        .mvcMatchers("/admin/**").hasAnyRole(EDITOR_ROLE, ADMIN_ROLE)
+                        .requestMatchers("/admin/**").hasAnyRole(EDITOR_ROLE, ADMIN_ROLE)
                         .anyRequest().permitAll()
                 )
                 .formLogin(login -> login
@@ -33,7 +33,7 @@ class CustomSecurityConfig {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().mvcMatchers(
+        return web -> web.ignoring().requestMatchers(
                 "/img/**",
                 "/scripts/**",
                 "/styles/**"
