28da50794d98660e5cc58aa2d43a5dd50e30a86d
Slawek Ludwiczak <slawekwch@gmail.com>
2023-02-07 11:54:29 +0100

    Spring Boot 3 - Lekcja konfiguracja uwierzytelnienia

--- 16-movieclub-uwierzytelnienie/src/main/java/pl/javastart/movieclub/config/security/CustomSecurityConfig.java
+++ 16-movieclub-uwierzytelnienie/src/main/java/pl/javastart/movieclub/config/security/CustomSecurityConfig.java
@@ -9,14 +9,14 @@ import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-class CustomSecurityConfig {
+public class CustomSecurityConfig {
     private static final String USER_ROLE = "USER";
     private static final String EDITOR_ROLE = "EDITOR";
     private static final String ADMIN_ROLE = "ADMIN";
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((authz) -> authz
-                        .mvcMatchers("/admin/**").hasAnyRole(EDITOR_ROLE, ADMIN_ROLE)
+                        .requestMatchers("/admin/**").hasAnyRole(EDITOR_ROLE, ADMIN_ROLE)
                         .anyRequest().permitAll()
                 )
                 .formLogin(withDefaults());
@@ -25,7 +25,7 @@ class CustomSecurityConfig {
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-        return web -> web.ignoring().mvcMatchers(
+        return web -> web.ignoring().requestMatchers(
                 "/img/**",
                 "/scripts/**",
                 "/styles/**"
