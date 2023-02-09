48f9f4fcb297f699ef380af07147b10661896a7f
Slawek Ludwiczak <slawekwch@gmail.com>
2023-02-09 08:38:30 +0100

    Spring Boot 3 - Lekcja dostep do konsoli h2

--- 21-movieclub-konsola-h2/src/main/java/pl/javastart/movieclub/domain/user/User.java
+++ 21-movieclub-konsola-h2/src/main/java/pl/javastart/movieclub/domain/user/User.java
@@ -1,6 +1,6 @@
 package pl.javastart.movieclub.domain.user;
 
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
--- 21-movieclub-konsola-h2/src/test/java/pl/javastart/movieclub/MovieclubApplicationTests.java
+++ 21-movieclub-konsola-h2/src/test/java/pl/javastart/movieclub/MovieclubApplicationTests.java
@@ -2,12 +2,41 @@ package pl.javastart.movieclub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = "spring.h2.console.enabled=true"
+)
+@ActiveProfiles("dev")
 class MovieclubApplicationTests {
+
+    @LocalServerPort
+    int port;
 
     @Test
     void contextLoads() {
+
     }
 
+    @Test
+    void shouldAllowAccessToH2Console() throws URISyntaxException, IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder(new URI("http://localhost:%s/h2-console/".formatted(port)))
+                .GET().build();
+        HttpClient client = HttpClient.newBuilder().followRedirects(HttpClient.Redirect.NORMAL)
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        int status = response.statusCode();
+        assertEquals(200, status);
+    }
 }
